0635368f926047a90d70db469a512d24dc6704a9
草鞋没号 <308487730@qq.com>
2019-10-29 09:17:07 +0800

    加入material-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -14,6 +15,8 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,24 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [ 
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
   ]
 })
 export class CoreModule {
