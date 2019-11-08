47ce9414a7552344c361cda7881bef740eeadebe
草鞋没号 <308487730@qq.com>
2019-11-08 21:32:44 +0800

    表单验证

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  handleSubmit({ value, valid }, ev: Event) {
+    ev.preventDefault();
+
+    console.log(value, valid);
   }
 
 }
